e5bb0016f6728ecc358bb9d06746388921bcd0f6
Hǎiliàng Wáng <w@h12.me>
2015-11-29 13:09:29 +0800

    test TopicMetadata

--- proto/proto_test.go
+++ proto/proto_test.go
@@ -1,7 +1,9 @@
 package proto
 
 import (
+	"math/rand"
 	"net"
+	"strconv"
 	"testing"
 
 	"h12.me/realtest/kafka"
@@ -18,28 +20,57 @@ func TestTopicMetadata(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer conn.Close()
-	topic, err := k.NewRandomTopic(0)
+	partitionCount := 2
+	topic, err := k.NewRandomTopic(partitionCount)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer k.DeleteTopic(topic)
-	reqMsg := &TopicMetadataRequest{
-		topic,
-	}
+	reqMsg := &TopicMetadataRequest{topic}
+	correlationID := rand.Int31()
 	req := Request{
 		APIKey:         reqMsg.APIKey(),
 		APIVersion:     reqMsg.APIVersion(),
-		CorrelationID:  1,
+		CorrelationID:  correlationID,
 		ClientID:       "abc",
 		RequestMessage: reqMsg,
 	}
 	if err := req.Send(conn); err != nil {
 		t.Fatal(t)
 	}
-	resp := &Response{
-		ResponseMessage: &TopicMetadataResponse{},
-	}
+	respMsg := &TopicMetadataResponse{}
+	resp := &Response{ResponseMessage: respMsg}
 	if err := resp.Receive(conn); err != nil {
 		t.Fatal(resp)
 	}
+	if resp.CorrelationID != correlationID {
+		t.Fatalf("correlation id: expect %d but got %d", correlationID, resp.CorrelationID)
+	}
+	for i := range brokers {
+		if respMsg.Brokers[i].Addr() != brokers[i] {
+			t.Fatalf("broker: expect %s but got %s", brokers[i], respMsg.Brokers[i].Addr())
+		}
+	}
+	if len(respMsg.TopicMetadatas) != 1 {
+		t.Fatalf("len(TopicMetadatas): expect 1 but got %d", len(respMsg.TopicMetadatas))
+	}
+	meta := &respMsg.TopicMetadatas[0]
+	if meta.ErrorCode != NoError {
+		t.Fatal(meta.ErrorCode)
+	}
+	if meta.TopicName != topic {
+		t.Fatalf("topic: expect %s but got %s", topic, meta.TopicName)
+	}
+	if len(meta.PartitionMetadatas) != partitionCount {
+		t.Fatalf("partition count: expect %d but got %d", partitionCount, len(meta.PartitionMetadatas))
+	}
+	for _, pMeta := range meta.PartitionMetadatas {
+		if pMeta.ErrorCode != NoError {
+			t.Fatal(pMeta.ErrorCode)
+		}
+	}
+}
+
+func (b *Broker) Addr() string {
+	return b.Host + ":" + strconv.Itoa(int(b.Port))
 }
