db97bccfb777e2753c8cb35bebd134642e2ae8a0
Hǎiliàng Wáng <w@h12.me>
2015-11-29 17:06:29 +0800

    move Send/Receive implementations to wipro

--- proto/error.go
+++ proto/error.go
@@ -3,12 +3,14 @@ package proto
 import (
 	"errors"
 	"fmt"
+
+	"h12.me/wipro"
 )
 
 var (
 	ErrSizeMismatch = errors.New("proto: size mismatch in response")
 	ErrCRCMismatch  = errors.New("proto: CRC mismatch in response")
-	ErrConn         = errors.New("proto: network connection error")
+	ErrConn         = wipro.ErrConn
 )
 
 func (code ErrorCode) Error() string {
--- proto/marshal.go
+++ proto/marshal.go
@@ -7,25 +7,9 @@ import (
 )
 
 func (req *Request) Send(conn io.Writer) error {
-	var w wipro.Writer
-	(&RequestOrResponse{M: req}).Marshal(&w)
-	if _, err := conn.Write(w.B); err != nil {
-		return ErrConn
-	}
-	return nil
+	return wipro.Send(&RequestOrResponse{M: req}, conn)
 }
 
 func (resp *Response) Receive(conn io.Reader) error {
-	r := wipro.Reader{B: make([]byte, 4)}
-	if _, err := conn.Read(r.B); err != nil {
-		return ErrConn
-	}
-	size := int(r.ReadInt32())
-	r.Grow(size)
-	if _, err := io.ReadAtLeast(conn, r.B[4:], size); err != nil {
-		return ErrConn
-	}
-	r.Reset()
-	(&RequestOrResponse{M: resp}).Unmarshal(&r)
-	return r.Err
+	return wipro.Receive(conn, &RequestOrResponse{M: resp})
 }
