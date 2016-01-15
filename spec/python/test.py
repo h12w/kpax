209a7665b0bbd7d87e8296a338dfc8b5d6d01307
Hǎiliàng Wáng <w@h12.me>
2016-01-15 09:52:39 +0800

    generate a better test case for further testing

--- spec/python/test.py
+++ spec/python/test.py
@@ -18,13 +18,21 @@ ProduceRequestPayload = namedtuple("ProduceRequestPayload",
     ["topic", "partition", "messages"])
 
 messages = [
-    create_snappy_message([("Snappy 1 %d" % i, None) for i in range(1)]),
-    create_snappy_message([("Snappy 2 %d" % i, None) for i in range(2)]),
+    create_snappy_message([("---start---", None), ("xxxxxxxxxxxxxxxxx", None)]),
+    create_snappy_message([("ccccc", None), ("---end---", None)]),
 ]
 
 produce = ProduceRequestPayload("topic1", 1, messages=messages)
 
-req = KafkaProtocol.encode_produce_request("abc", 2, payloads=[produce], acks=1, timeout=1000)
-print req
-#print len(req)
-#print "[]byte{" +''.join( [ "0x%02x, " % ord( x ) for x in req ] ).strip() + "}"
+correlationID = 3
+req = KafkaProtocol.encode_produce_request("clientID", correlationID, payloads=[produce], acks=1, timeout=1000)
+
+print "[]byte{" +''.join( [ "0x%02x, " % ord( x ) for x in req ] ).strip() + "}"
+
+# python test.py | hexdump -C
+import subprocess
+task = subprocess.Popen("hexdump -C", shell=True, stdin=subprocess.PIPE)
+task.stdin.write(req)
+task.stdin.flush()
+task.stdin.close()
+task.wait()
