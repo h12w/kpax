9eeccf50145b32bd3df76293e9c95eb41fc6753b
Hǎiliàng Wáng <w@h12.me>
2016-06-21 14:17:07 +0800

    expose the original error

--- proto/error.go
+++ proto/error.go
@@ -3,14 +3,11 @@ package proto
 import (
 	"errors"
 	"fmt"
-
-	"h12.me/wipro"
 )
 
 var (
 	ErrSizeMismatch = errors.New("proto: size mismatch in response")
 	ErrCRCMismatch  = errors.New("proto: CRC mismatch in response")
-	ErrConn         = wipro.ErrConn
 )
 
 func (code ErrorCode) Error() string {
