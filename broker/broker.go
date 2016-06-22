d50c5efbfa373632e509dda924bca990b929ace7
Hǎiliàng Wáng <w@h12.me>
2016-06-22 16:23:45 +0800

    reimplement broker

--- broker/broker.go
+++ broker/broker.go
@@ -10,31 +10,13 @@ import (
 	"h12.me/kpax/model"
 )
 
-var (
-	ErrCorrelationIDMismatch = errors.New("correlationID mismatch")
-)
-
 type B struct {
 	Addr     string
 	Timeout  time.Duration
 	QueueLen int
 
-	connection
-	cid      int32
-	recvChan chan *brokerJob
-	mu       sync.Mutex
-}
-
-type connection struct {
-	conn net.Conn
-	mu   sync.Mutex
-	*B
-}
-
-type brokerJob struct {
-	req     model.Request
-	resp    model.Response
-	errChan chan error
+	br *broker
+	mu sync.Mutex
 }
 
 func New(addr string) *B {
@@ -43,13 +25,66 @@ func New(addr string) *B {
 		Timeout:  30 * time.Second,
 		QueueLen: 1000,
 	}
-	b.connection.B = b
 	return b
 }
 
 func NewDefault(addr string) model.Broker { return New(addr) }
 
 func (b *B) Do(req model.Request, resp model.Response) error {
+	b.mu.Lock()
+	if b.br == nil {
+		var err error
+		b.br, err = b.newBroker()
+		if err != nil {
+			b.mu.Unlock()
+			return err
+		}
+	}
+	b.mu.Unlock()
+
+	if err := b.br.do(req, resp); err != nil {
+		b.Close()
+		return err
+	}
+	return nil
+}
+
+func (b *B) Close() {
+	b.mu.Lock()
+	b.br.close()
+	b.br = nil
+	b.mu.Unlock()
+}
+
+type broker struct {
+	timeout  time.Duration
+	conn     net.Conn
+	recvChan chan *brokerJob
+	cid      int32
+	mu       sync.Mutex
+}
+
+type brokerJob struct {
+	req     model.Request
+	resp    model.Response
+	errChan chan error
+}
+
+func (b *B) newBroker() (*broker, error) {
+	conn, err := net.DialTimeout("tcp", b.Addr, b.Timeout)
+	if err != nil {
+		return nil, err
+	}
+	br := &broker{
+		timeout:  b.Timeout,
+		conn:     conn,
+		recvChan: make(chan *brokerJob, b.QueueLen),
+	}
+	go br.receiveLoop()
+	return br, nil
+}
+
+func (b *broker) do(req model.Request, resp model.Response) error {
 	job := &brokerJob{
 		req:     req,
 		resp:    resp,
@@ -58,83 +93,56 @@ func (b *B) Do(req model.Request, resp model.Response) error {
 	if err := b.send(job); err != nil {
 		return err
 	}
-	return <-job.errChan
+	if job.requireAck() {
+		return <-job.errChan
+	}
+	return nil
 }
 
-func (b *B) send(job *brokerJob) error {
-	conn, err := b.getConn()
-	if err != nil {
-		return err
-	}
-
+func (b *broker) send(job *brokerJob) error {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 	job.req.SetID(atomic.AddInt32(&b.cid, 1))
-	if err := job.req.Send(conn); err != nil {
-		b.closeConn()
+	if err := b.conn.SetWriteDeadline(time.Now().Add(b.timeout)); err != nil {
 		return err
 	}
-	if b.recvChan == nil {
-		b.recvChan = make(chan *brokerJob, b.QueueLen)
-		go b.receiveLoop()
+	if err := job.req.Send(b.conn); err != nil {
+		return err
 	}
 	if job.requireAck() {
 		b.recvChan <- job
 	}
 	return nil
 }
-func (j *brokerJob) requireAck() bool { return j.resp != nil }
 
-func (c *B) receiveLoop() {
-	for job := range c.recvChan {
-		conn, err := c.getConn()
-		if err != nil {
+var (
+	errCorrelationIDMismatch = errors.New("correlationID mismatch")
+)
+
+func (b *broker) receiveLoop() {
+	for job := range b.recvChan {
+		if err := b.conn.SetReadDeadline(time.Now().Add(b.timeout)); err != nil {
 			job.errChan <- err
 			continue
 		}
-		if err := job.resp.Receive(conn); err != nil {
-			c.closeConn()
+		if err := job.resp.Receive(b.conn); err != nil {
 			job.errChan <- err
 			continue
 		}
 		if job.resp.ID() != job.req.ID() {
-			c.closeConn()
-			job.errChan <- ErrCorrelationIDMismatch
+			job.errChan <- errCorrelationIDMismatch
 			continue
 		}
 		job.errChan <- nil
 	}
-	c.closeConn()
+	// no need to closeConn here because when recvChan closed, the receiveLoop will do it.
+	b.conn.Close()
+	b.conn = nil
 }
 
-func (b *B) Close() {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-	if b.recvChan != nil {
-		close(b.recvChan)
-		b.recvChan = nil
-	}
-	b.closeConn()
+func (b *broker) close() {
+	close(b.recvChan)
+	b.recvChan = nil
 }
 
-func (c *connection) getConn() (net.Conn, error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	if c.conn == nil {
-		conn, err := net.DialTimeout("tcp", c.Addr, c.Timeout)
-		if err != nil {
-			return nil, err
-		}
-		c.conn = conn
-	}
-	return c.conn, c.conn.SetDeadline(time.Now().Add(c.Timeout))
-}
-
-func (c *connection) closeConn() {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	if c.conn != nil {
-		c.conn.Close()
-		c.conn = nil
-	}
-}
+func (j *brokerJob) requireAck() bool { return j.resp != nil }
